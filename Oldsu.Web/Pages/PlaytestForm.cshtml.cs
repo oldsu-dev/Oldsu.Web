ab1879c1486c21617050f73a34b4ff46cbc9217b
jvyden <jvyden@jvyden.xyz>
2021-09-06 11:15:17 -0400

    Add (empty) playtest form

---
+++ Oldsu.Web/Pages/PlaytestForm.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Oldsu.Web.Pages {
+    public class PlaytestForm : BaseLayout {
+        public void OnGet() {
+            
+        }
+    }
+}
