4b1159d6f6e7f4686edd145ca383726b5271cc31
jvyden <jvyden@jvyden.xyz>
2021-09-11 09:02:43 -0400

    add runtime compilation for debug builds

--- Oldsu.Web/Startup.cs
+++ Oldsu.Web/Startup.cs
@@ -16,9 +16,12 @@ namespace Oldsu.Web
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
+        public void ConfigureServices(IServiceCollection services) {
+            #if DEBUG
+                services.AddRazorPages().AddRazorRuntimeCompilation();
+            #else
+                services.AddRazorPages();
+            #endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
