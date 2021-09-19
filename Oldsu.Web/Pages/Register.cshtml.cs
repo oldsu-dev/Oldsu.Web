40924e8ff1158aef339accdd1c6208bd855292e6
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-09-19 11:00:23 +0300

    Add RegisterSubmitModel and unfinished register handlers.

--- Oldsu.Web/Pages/Register.cshtml.cs
+++ Oldsu.Web/Pages/Register.cshtml.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Oldsu.Web.Models;
 
 namespace Oldsu.Web.Pages
 {
-    public class Register : PageModel
+    [IgnoreAntiforgeryToken]
+    public class Register : BaseLayout
     {
+        public string RegistrationResult { get; set; }
+
         public void OnGet()
         {
+           Console.WriteLine("received get");
+           RegistrationResult = "joo";
+        }
+        
+        public async Task OnPost(RegisterSubmitModel registerData)
+        {
+            await using var database = new Database();
             
+            //database.RegisterAsync()
         }
     }
 }
