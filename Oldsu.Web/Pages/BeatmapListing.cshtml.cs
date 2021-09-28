7b3394f3389d4a0d168abf83bebf226a1f7b4c46
jvyden <jvyden@jvyden.xyz>
2021-09-28 00:34:41 -0400

    working search in beatmap listing

--- Oldsu.Web/Pages/BeatmapListing.cshtml.cs
+++ Oldsu.Web/Pages/BeatmapListing.cshtml.cs
@@ -1,17 +1,25 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Oldsu.Types;
 
 namespace Oldsu.Web.Pages {
     public class BeatmapListing : BaseLayout {
         public Beatmapset[]? BeatmapSets { get; private set; }
+
+        [FromQuery(Name = "query")] public string? SearchQuery { get; set; } = string.Empty;
         
         public async Task<IActionResult> OnGet() {
             await using Database database = new();
 
-            this.BeatmapSets = await database.Beatmapsets.ToArrayAsync();
+            if(!string.IsNullOrEmpty(SearchQuery)) {
+                this.BeatmapSets = await database.Beatmapsets.Where(s => s.Title.Contains(SearchQuery) || s.Artist.Contains(SearchQuery)).ToArrayAsync();
+            }
+            else {
+                this.BeatmapSets = await database.Beatmapsets.ToArrayAsync();
+            }
+
             
             return this.Page();
         }
