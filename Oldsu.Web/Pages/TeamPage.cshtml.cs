2992b92b88065bd5999861722f607b9ff4f48f1e
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-03 13:18:44 +0300

    Add TeamPage cshtml and profile picture on navbar.

---
+++ Oldsu.Web/Pages/TeamPage.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Oldsu.Web.Pages
+{
+    public class TeamPage : BaseLayout
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
