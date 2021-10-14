0eca4195ed518a1019c2a93ef6e4e940f9788d3a
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-14 15:58:52 +0300

    Move SessionIdProvider from Login to a seperate class.

---
+++ Oldsu.Web/Utils/SessionIdProvier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Oldsu.Web.Utils
+{
+    public class SessionIdProvider
+    {
+        public static string GetSessionId(uint length)
+        {
+            using var rng = new RNGCryptoServiceProvider();
+            var buffer = new byte[length/2];
+            rng.GetBytes(buffer);
+
+            return BitConverter.ToString(buffer).Replace("-", "");
+        }
+    }
+}
