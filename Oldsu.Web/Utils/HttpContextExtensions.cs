3b3a8e737f40609c233a8d61107a920bfad93bbe
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-14 15:59:29 +0300

    Add HttpContextExtensions.

---
+++ Oldsu.Web/Utils/HttpContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Oldsu.Web.Utils
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetIpAddress(this HttpContext context)
+        {
+            if (!string.IsNullOrEmpty(context.Request.Headers["CF-CONNECTING-IP"]))
+                return context.Request.Headers["CF-CONNECTING-IP"];
+
+            var ipAddress = context.GetServerVariable("HTTP_X_FORWARDED_FOR");
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                var addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                    return addresses.Last();
+            }
+
+            return context.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
