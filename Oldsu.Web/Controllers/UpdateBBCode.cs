0e6cb8ccb2c74234533e4dc9722eeeffd0b65a74
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-16 14:26:40 +0300

    Add UpdateBBCode and UpdateInformation controllers.

---
+++ Oldsu.Web/Controllers/UpdateBBCode.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Oldsu.Web.Models;
+
+namespace Oldsu.Web.Controllers
+{
+    [ApiController]
+    [IgnoreAntiforgeryToken]
+    [Route("/update_bbcode")]
+    public class UpdateBBCode : Controller
+    {
+        [HttpPut]
+        public async Task<IActionResult> OnPut([FromForm] BBCodeUpdateModel updatedBBCode)
+        {
+            await using var db = new Database(); // todo move this to middleware
+
+            var hasCookie = Request.Cookies.TryGetValue("oldsu-sid", out var cookie);
+
+            if (!hasCookie)
+                return Unauthorized();
+
+            var userSession = await db.GetWebSession(cookie);
+
+            if (userSession == null)
+                return Unauthorized();
+
+            await db.UpdateBBCode(userSession.UserID, updatedBBCode.BBCode);
+            
+            return Ok();
+        }
+    }
+}
