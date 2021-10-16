d9e9ce81d5686f04399bbbbeb7b89b6fa0426531
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-16 14:26:40 +0300

    Add UpdateBBCode and UpdateInformation controllers.

---
+++ Oldsu.Web/Controllers/UpdateInformation.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Oldsu.Web.Models;
+
+namespace Oldsu.Web.Controllers
+{
+    [ApiController]
+    [IgnoreAntiforgeryToken]
+    [Route("/update_information")]
+    public class UpdateInformation : Controller
+    {
+        [HttpPut]
+        public async Task<IActionResult> OnPut([FromForm] InformationUpdateModel updatedInformation)
+        {
+            await using var db = new Database(); // todo move this to middleware
+
+            var hasCookie = Request.Cookies.TryGetValue("oldsu-sid", out var cookie);
+
+            if (!hasCookie)
+                return Unauthorized();
+
+            var userSession = await db.GetWebSession(cookie);
+
+            if (userSession == null)
+                return Unauthorized();
+
+            await db.UpdateInformation(userSession.UserID, updatedInformation.Occupation, updatedInformation.Interests,
+                updatedInformation.Birthday, updatedInformation.Discord, updatedInformation.Twitter,
+                updatedInformation.Website);
+
+            return Ok();
+        }
+    }
+}
---
+++ Oldsu.Web/Models/InformationUpdateModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Oldsu.Web.Models
+{
+    public class InformationUpdateModel
+    {
+        [BindProperty(Name = "occupation")]
+        [StringLength(100)]
+        public string? Occupation { get; set; }
+        
+        [BindProperty(Name = "interests")]
+        [StringLength(100)]
+        public string? Interests { get; set; }
+        
+        [BindProperty(Name = "age")]
+        public DateTime? Birthday { get; set; }
+        
+        [BindProperty(Name = "discord")]
+        [StringLength(100)]
+        public string? Discord { get; set; }
+        
+        [BindProperty(Name = "twitter")]
+        [StringLength(100)]
+        public string? Twitter { get; set; }
+        
+        [BindProperty(Name = "website")]
+        [StringLength(100)]
+        public string? Website { get; set; }
+    }
+}
