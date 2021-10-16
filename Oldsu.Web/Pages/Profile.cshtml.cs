af1dc4c40f8cf9f791c739fe4b157f31ee92a29e
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-16 14:24:53 +0300

    Add Profile page to for redirection.

---
+++ Oldsu.Web/Pages/Profile.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Oldsu.Web.Pages
+{
+    public class Profile : BaseLayout
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await AuthenticateUserSession();
+
+            if (AuthenticatedUserInfo != null)
+                return Redirect($"/u/{AuthenticatedUserInfo.UserID}");
+
+            return Redirect("/");
+        }
+    }
+}
