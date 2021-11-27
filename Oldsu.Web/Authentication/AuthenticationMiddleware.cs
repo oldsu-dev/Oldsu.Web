633b36bd93c7edce9bc1825c17bbae15bdc86d7f
Jiniux <j3nial@icloud.com>
2021-11-27 14:43:24 +0100

    Fix AuthenticationMiddleware.cs early return

--- Oldsu.Web/Authentication/AuthenticationMiddleware.cs
+++ Oldsu.Web/Authentication/AuthenticationMiddleware.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Oldsu.Types;
@@ -18,22 +19,18 @@ namespace Oldsu.Web.Authentication
         {
             await using var db = new Database();
 
-            if (!context.Request.Cookies.TryGetValue("oldsu-sid", out var sessionId))
-                return;
-
-            UserInfo? userInfo = await SessionAuthenticator.Authenticate(sessionId!);
-
-            if (userInfo == null)
+            if (context.Request.Cookies.TryGetValue("oldsu-sid", out var sessionId))
             {
-                context.Response.Cookies.Delete("oldsu-sid");
-                return;
+                UserInfo? userInfo = await SessionAuthenticator.Authenticate(sessionId!);
+
+                if (userInfo != null)
+                    authenticationService.AuthenticatedUserInfo = userInfo;
+                else
+                    context.Response.Cookies.Delete("oldsu-sid");
             }
 
-            authenticationService.AuthenticatedUserInfo = userInfo;
-            
             // Call the next delegate/middleware in the pipeline
             await _next(context);
         }
-
     }
 }
